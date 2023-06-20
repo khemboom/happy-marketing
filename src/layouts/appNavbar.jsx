d165d47977ef52aa1a7de91b2f73fc3b2689f275
kem_harold <kemharold.garate@raybiztech.com>
2023-06-20 10:52:58 +0800

    fix responsive issue

--- src/layouts/appNavbar.jsx
+++ src/layouts/appNavbar.jsx
@@ -4,21 +4,24 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const AppNavbar = ({ scrollToSection }) => {
   return (
     <>
-      <Navbar className="navbar" bg="dark" variant="dark">
+      <Navbar expand="lg" className="navbar" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">Happy Marketing</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link onClick={() => scrollToSection("ourApproachSection")}>
-              Our Approach
-            </Nav.Link>
-            <Nav.Link onClick={() => scrollToSection("ourServicesSection")}>
-              Services
-            </Nav.Link>
-            <Nav.Link onClick={() => scrollToSection("aboutUsSection")}>
-              About Us
-            </Nav.Link>
-            <Nav.Link>Contact Us</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => scrollToSection("ourApproachSection")}>
+                Our Approach
+              </Nav.Link>
+              <Nav.Link onClick={() => scrollToSection("ourServicesSection")}>
+                Services
+              </Nav.Link>
+              <Nav.Link onClick={() => scrollToSection("aboutUsSection")}>
+                About Us
+              </Nav.Link>
+              <Nav.Link>Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
--- src/sections/contactUs.jsx
+++ src/sections/contactUs.jsx
@@ -18,30 +18,30 @@ const ContactUs = () => {
 
   return (
     <div className='contact-us-section'>
-      <Container className='contact-us-details'>
-        <Row>
+      <Container>
+        <Row className='contact-us-row'>
           <Col className='pt-2 pb-2'>
             <div className='contact-details-form'>
-            <Form onSubmit={sendEmail}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Complete Name</Form.Label>
-                <Form.Control type="text" name="user_name" placeholder="Your Name" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name="user_email" placeholder="email@example.com" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Example textarea</Form.Label>
-                <Form.Control as="textarea" name="message" placeholder='Message' rows={3} />
-              </Form.Group>
-              <Button variant="primary" type="submit" value="Send">Send</Button>
-            </Form>
+              <Form onSubmit={sendEmail}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Complete Name</Form.Label>
+                  <Form.Control type="text" name="user_name" placeholder="Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" name="user_email" placeholder="email@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Example textarea</Form.Label>
+                  <Form.Control as="textarea" name="message" placeholder='Message' rows={3} />
+                </Form.Group>
+                <Button variant="primary" type="submit" value="Send">Send</Button>
+              </Form>
             </div>
           </Col>
           <Col>
             <ul className='mt-4'>
-              <li>
+              <li className="text-white">
                 Copyright{' '}
                 <FontAwesomeIcon icon={faCopyright} />
                 <span>{' '}2023 "Happy Digital Marketing Agency" All rights reserved</span>
